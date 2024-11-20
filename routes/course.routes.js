507394243f994de5544372f9949bebc6cc847964
mohitsolanki026 <mohitsolanki06123@gmail.com>
2024-11-20 21:38:07 +0530

    Add course creation functionality with validation

--- routes/course.routes.js
+++ routes/course.routes.js
@@ -6,5 +6,6 @@ const router = express.Router()
 router.get("/all", auth, controller.getCourses)
 router.get("/my", auth, controller.getMyCourses)
 router.get("/:id", auth, controller.getCourse)
+router.post("/", auth, controller.createCourse)
 
 export default router;
--- validations/course.validations.js
+++ validations/course.validations.js
@@ -4,6 +4,4 @@ export const createCourseValidation = Joi.object({
     title: Joi.string().required(),
     description: Joi.string().required(),
     price: Joi.number().required(),
-    clientId: Joi.string().required(),
-    generalLinks: Joi.array().items(Joi.string())
 });
