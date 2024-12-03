3dfe9b5658ba8b146964e093a5c86861477f8209
mohitsolanki026 <mohitsolanki06123@gmail.com>
2024-12-03 23:14:50 +0530

    Add payment Gatway of PhonePe

--- routes/index.js
+++ routes/index.js
@@ -2,11 +2,13 @@ import express from 'express';
 import authRoutes from './auth.routes.js';
 import meetRoutes from './meet.routes.js';
 import courseRouter from './course.routes.js';
+import paymentRoutes from './payment.routes.js';
 
 const router = express.Router();
 
 router.use('/auth', authRoutes);
 router.use('/meet', meetRoutes);
 router.use('/course', courseRouter);
+router.use('/payment', paymentRoutes);
 
 export default router;
